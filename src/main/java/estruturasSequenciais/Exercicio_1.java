a39cda4d3a16a07403ca46937663a54a1512a99a
Melquias <melquias.c.oliveira@gmail.com>
2025-05-12 11:34:20 -0300

    problema_1

---
+++ src/main/java/estruturasSequenciais/Exercicio_1.java
@@ -0,0 +1,23 @@
+package estruturasSequenciais;
+
+public class Exercicio_1 {
+	
+	/* PROBLEMA - TERRENO:
+	 
+	 Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma
+	 casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais. Em seguida,
+	 o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com
+	 duas casas decimais.
+	 
+	 Exemplo:
+	 
+	 Digite a largura do terreno: 10.0
+	 Digite o comprimento do terreno: 30.0
+	 Digite o valor do metro quadrado: 200.00
+	 
+	 AREA DO TERRENO = 300.00
+	 PRECO DO TERRENO = 60000.00
+	 
+	 */
+
+}
---
+++ src/main/java/estruturasSequenciais/Resolucao_1.java
@@ -0,0 +1,41 @@
+package estruturasSequenciais;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Resolucao_1 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double width, length, squareMeter;
+		
+		System.out.print("Enter the width of the land: ");
+		width = sc.nextDouble();
+		System.out.print("Enter the length of the land: ");
+		length = sc.nextDouble();
+		System.out.print("value per square meter: ");
+		squareMeter = sc.nextDouble();
+		
+		double landArea = area(width, length);
+		double landPrice = price(landArea, squareMeter);
+		
+		System.out.printf("\nLAND AREA = %.2f", landArea);
+		System.out.printf("\nLAND PRICE = %.2f", landPrice);
+		
+		
+		sc.close();
+
+	}
+	
+	public static double area(double width, double length) {
+		return width * length;
+	}
+	
+	public static double price(double area, double squareMeter) {
+		return area * squareMeter;
+	}
+
+}
