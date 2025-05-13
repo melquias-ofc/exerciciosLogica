0247c9bc01d6f65e86495c00ca6208f514ed0e2a
Melquias <melquias.c.oliveira@gmail.com>
2025-05-13 09:54:43 -0300

    exercicio_2 - estrutura sequencial

---
+++ src/main/java/estruturasSequenciais/Exercicio_2.java
@@ -0,0 +1,20 @@
+package estruturasSequenciais;
+
+public class Exercicio_2 {
+
+	/* PROBLEMA - RETANGULO:
+	 
+	 Fazer um programa para ler as medidas da base e da altura de um retângulo. Em seguida, mostrar o valor
+	 da área, perímetro e diagonal deste retângulo, com quatro casas decimais, conforme exemplo.
+	 
+	 Exemplo:
+	 
+	 Base do retangulo: 4.0
+	 Altura do retangulo: 5.0
+	 
+	 AREA = 20.0000
+	 PERIMETRO = 18.0000
+	 DIAGONAL = 6.4031
+	 
+	 */
+}
