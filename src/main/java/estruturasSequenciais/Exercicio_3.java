1b38ef988e37261c91fa1804df1b6621b3460883
Melquias <melquias.c.oliveira@gmail.com>
2025-05-14 09:43:13 -0300

    exercicio_3 - estrutura sequencial

---
+++ src/main/java/estruturasSequenciais/Exercicio_3.java
@@ -0,0 +1,23 @@
+package estruturasSequenciais;
+
+public class Exercicio_3 {
+
+	/* PROBLEMA - TERRENO:
+	 
+	 Fazer um programa para ler o nome e idade de duas pessoas. Ao final mostrar uma mensagem com os
+	 nomes e a idade média entre essas pessoas, com uma casa decimal, conforme exemplo. 
+
+	 Exemplo:
+	 
+	 Dados da primeira pessoa:
+	 Nome: Marial Almeida
+	 Idade: 19
+
+	 Dados da segunda pessoa:
+	 Nome: Joao Sousa
+	 Idade 20
+
+	 A idade média de Maria Almeida e Joao Sousa é de 19.5 anos 
+	 
+	 */
+}
