2a0ce90427390418ee0f80e33317642073a55813
Melquias <melquias.c.oliveira@gmail.com>
2025-05-15 10:55:35 -0300

    exercicio_5 - estrutura sequencial

---
+++ src/main/java/estruturasSequenciais/Exercicio_5.java
@@ -0,0 +1,21 @@
+package estruturasSequenciais;
+
+public class Exercicio_5 {
+	
+	/* PROBLEMA - TROCO:
+	 
+	 Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia. 
+	 O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto, 
+	 e o valor em dinheiro dado pelo cliente (suponha que haja dinheiro suficiente). Seu programa deve 
+	 mostrar o valor do troco a ser devolvido ao cliente.
+	 
+	 Exemplo:
+	 
+	 Preço unitário do produto: 8.00 
+	 Quantidade comprada: 2 
+	 Dinheiro recebido: 20.00 
+	
+	 TROCO = 4.00
+	 
+	 */
+}
