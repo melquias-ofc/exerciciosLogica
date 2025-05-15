9ab105b166db655bd5af5117bd277c4fce2abd4a
Melquias <melquias.c.oliveira@gmail.com>
2025-05-15 10:15:20 -0300

    exercicio_4 - estrutura sequencial

---
+++ src/main/java/estruturasSequenciais/Exercicio_4.java
@@ -0,0 +1,18 @@
+package estruturasSequenciais;
+
+public class Exercicio_4 {
+	
+	/* PROBLEMA - TERRENO:
+	 
+	 Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela o valor da soma destes 
+	 números.
+	 
+	 Exemplo:
+	 
+	 Digite o valor de X: 8 
+	 Digite o valor de Y: 10 
+	 
+	 SOMA = 18 
+	 
+	 */
+}
