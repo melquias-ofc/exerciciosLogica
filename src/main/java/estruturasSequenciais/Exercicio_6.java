0ddc17a95723313f5834bee967bc846f66700d30
Melquias <melquias.c.oliveira@gmail.com>
2025-05-16 15:04:10 -0300

    exercicio_6 - estrutura sequencial

---
+++ src/main/java/estruturasSequenciais/Exercicio_6.java
@@ -0,0 +1,19 @@
+package estruturasSequenciais;
+
+public class Exercicio_6 {
+	
+	/* PROBLEMA - CIRCULO:
+	 
+	 Fazer um programa para ler o valor "r" do raio de um círculo, e depois mostrar o valor da área do 
+	 círculo com três casas decimais. A fórmula da área do círculo é a seguinte: 𝑎𝑟𝑒𝑎 = 𝜋.𝑟 . Você pode 
+	 usar o valor de 𝜋 fornecido pela biblioteca da sua linguagem de programação, ou então, se preferir,
+	 use diretamente o valor 3.14159.
+	 
+	 Exemplo:
+	 
+	 Digite o valor do raio do circulo: 13.2 
+	 
+	 AREA = 547.391 
+	 
+	 */
+}
