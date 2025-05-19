059271530fc9118a56ac25a2d230765b8415e369
Melquias <melquias.c.oliveira@gmail.com>
2025-05-19 10:55:00 -0300

    exercicio_10 - estrutura sequencial

---
+++ src/main/java/estruturasSequenciais/Exercicio_10.java
@@ -0,0 +1,19 @@
+package estruturasSequenciais;
+
+public class Exercicio_10 {
+
+	/* PROBLEMA - DURAÇÃO:
+	 
+	 Fazer um programa para ler uma duração de tempo em segundos, daí imprimir na tela esta duração no 
+	 formato horas:minutos:segundos.
+	 
+	 Exemplo:
+	 
+	 Digite a duracao em segundos: 300 
+	 0:5:0 
+	 
+	 Digite a duracao em segundos: 140811 
+	 39:6:51 
+	 
+	 */
+}
---
+++ src/main/java/estruturasSequenciais/Exercicio_8.java
@@ -0,0 +1,18 @@
+package estruturasSequenciais;
+
+public class Exercicio_8 {
+
+	/* PROBLEMA - CONSUMO:
+	 
+	 Fazer um programa para ler a distância total (em km) percorrida por um carro, bem como o total de 
+	 combustível gasto por este carro ao percorrer tal distância. Seu programa deve mostrar o consumo 
+	 médio do carro, com três casas decimais. 
+	 
+	 Exemplo:
+	 
+	 Distancia percorrida: 1108 
+	 Combustível gasto: 71.4 
+	 Consumo medio = 15.518
+	 
+	 */
+}
