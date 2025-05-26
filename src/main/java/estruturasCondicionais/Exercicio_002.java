5c9b8ecca2998f6c06f68ffc349787ac90544eb4
Melquias <melquias.c.oliveira@gmail.com>
2025-05-26 13:54:30 -0300

    exercicio_002 - estrutura condicional

---
+++ src/main/java/estruturasCondicionais/Exercicio_002.java
@@ -0,0 +1,38 @@
+package estruturasCondicionais;
+
+public class Exercicio_002 {
+
+	/* PROBLEMA - BASKARA:
+	 
+	 Fazer um programa para ler os três coeficientes de uma equação do segundo grau. Usando a fórmula
+	 de Baskara, calcular e mostrar os valores das raízes x1 e x2 da equação com quatro casas decimais,
+	 conforme exemplo. Se a equação não possuir raízes reais, mostrar uma mensagem. 
+
+	 Exemplo 1:
+	 
+	 Coeficiente a: 1
+	 Coeficiente b: 0
+	 Coeficiente c: -9
+	 
+	 X1 = 3.0000
+	 X2 = -3.0000
+	 
+	 Exemplo 2:
+
+	 Coeficiente a: 2
+	 Coeficiente b: -4.5
+	 Coeficiente c: 1.7
+	 
+	 X1 = 1.7697
+	 X2 = 0.4803 
+	 
+	 Exemplo 3:
+	 
+	 Coeficiente a: 1
+	 Coeficiente b: 3
+	 Coeficiente c: 4
+	 
+	 NAO POSSUI RAIZES REAIS
+	 
+	 */
+}
