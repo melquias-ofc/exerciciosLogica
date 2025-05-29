5fc76126f923b575a96e37a8c54211b31cae4de7
Melquias <melquias.c.oliveira@gmail.com>
2025-05-29 10:04:37 -0300

    exercicio_004 - estrutura condicional

---
+++ src/main/java/estruturasCondicionais/Exercicio_004.java
@@ -0,0 +1,22 @@
+package estruturasCondicionais;
+
+public class Exercicio_004 {
+
+	/* PROBLEMA - OPERADORA:
+	 
+	 Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de 
+	 telefone. Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00. Fazer um programa para 
+	 ler quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago. 
+
+	 Exemplo 1: 
+	
+	 Digite a quantidade de minutos: 22 
+	 Valor a pagar: R$ 50.00 
+	 
+	 Exemplo 2: 
+	 
+ 	 Digite a quantidade de minutos: 103 
+	 Valor a pagar: R$ 56.00
+	 
+	 */
+}
