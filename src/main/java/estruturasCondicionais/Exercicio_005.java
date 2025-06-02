1f177b7cb9fc4b0f729cbe66ef6758803932da1a
Melquias <melquias.c.oliveira@gmail.com>
2025-06-01 21:59:03 -0300

    exercicio_005 - estrutura condicional

---
+++ src/main/java/estruturasCondicionais/Exercicio_005.java
@@ -0,0 +1,30 @@
+package estruturasCondicionais;
+
+public class Exercicio_005 {
+
+	/* PROBLEMA - TROCO VERIFICADO:
+	 
+	 Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia. 
+	 O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto, 
+	 e o valor em dinheiro dado pelo cliente. Seu programa deve mostrar o valor do troco a ser devolvido 
+	 ao cliente. Se o dinheiro dado pelo cliente não for suficiente, mostrar uma mensagem informando o 
+	 valor restante conforme exemplo.
+	 
+	 Exemplo 1: 
+	 
+	 Preço unitário do produto: 8.00 
+	 Quantidade comprada: 2 
+	 Dinheiro recebido: 20.00 
+	
+	 TROCO = 4.00
+	 
+	 Exemplo 2: 
+	 
+	 Preço unitário do produto: 30.00 
+	 Quantidade comprada: 3 
+	 Dinheiro recebido: 70.00 
+	
+	 DINHEIRO INSUFICIENTE. FALTAM 20.00 REAIS
+	 
+	 */
+}
