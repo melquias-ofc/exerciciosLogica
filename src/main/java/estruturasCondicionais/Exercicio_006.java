a52e3dee160d4b5771a3ef85dd549aa15f12466e
Melquias <melquias.c.oliveira@gmail.com>
2025-06-03 20:42:50 -0300

    exercicio_006 - estrutura condicional

---
+++ src/main/java/estruturasCondicionais/Exercicio_006.java
@@ -0,0 +1,28 @@
+package estruturasCondicionais;
+
+public class Exercicio_006 {
+
+	/* PROBLEMA - GLICOSE:
+	 
+	 Fazer um programa para ler a quantidade de glicose no sangue de uma pessoa e depois 
+	 mostrar na tela a classificação desta glicose de acordo com a tabela de referência.
+	 
+	 Classificação      Glicose 
+       Normal        Até 100 mg/dl 
+       Elevado       Maior que 100 até 140 mg/dl 
+       Diabetes      Maior de 140 mg/dl 
+     
+     Exemplo 1: 
+	 Digite a medida da glicose: 90.0 
+	 Classificacao: normal 
+ 
+	 Exemplo 2: 
+	 Digite a medida da glicose: 140.0 
+	 Classificacao: elevado 
+ 
+	 Exemplo 3: 
+	 Digite a medida da glicose: 143.2 
+	 Classificacao: diabetes 
+	 
+	 */
+}
